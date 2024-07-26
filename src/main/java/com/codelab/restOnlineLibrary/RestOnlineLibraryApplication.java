b5c5fa4298329508c03fe3eb927e20c7406b6125
bmribeiro <bruno.miguel.rib@gmail.com>
2024-07-26 19:38:12 +0100

    Initial Commit

---
+++ src/main/java/com/codelab/restOnlineLibrary/RestOnlineLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.restOnlineLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOnlineLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOnlineLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/restOnlineLibrary/RestOnlineLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.restOnlineLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOnlineLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
